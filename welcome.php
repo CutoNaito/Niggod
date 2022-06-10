56b3a2c1affda6fb8bfa3e7eaad4f200470def26
NNemec3 <neightn7@gmail.com>
2022-06-10 23:37:29 +0200

    Add lsk

--- welcome.php
+++ welcome.php
@@ -15,6 +15,14 @@ if (isset($_SESSION["logged"]) && $_SESSION["logged"] === true) {
     <link rel="icon" href="img/NiggodRat.ico">
     <link rel="stylesheet" href="css/welcome.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+        main {
+            background-image: url("img/lsk.gif");
+            background-size: cover;
+            height: 1000px;
+        }
+    </style>
 </head>
 
 <body>
@@ -41,22 +49,7 @@ if (isset($_SESSION["logged"]) && $_SESSION["logged"] === true) {
             </div>
         </nav>
     </header>
-
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <a href="user/login.php" id="log-welcome">Log In</a>
-                    <p>or</p>
-                    <a href="user/signup.php" id="sign-welcome">Sign Up</a>
-                </div>
-            </div>
-        </div>
     </main>
-    <footer>
-
-
-    </footer>
 </body>
-
 </html>
