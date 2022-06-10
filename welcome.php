60b1833b260d9a7c6a2391a01af3a41f337cfb7d
NNemec3 <neightn7@gmail.com>
2022-06-10 20:57:43 +0200

    AddNav
    + Navbar with:
    ++ Logo
    ++ Buttons

--- welcome.php
+++ welcome.php
@@ -8,28 +8,55 @@ if (isset($_SESSION["logged"]) && $_SESSION["logged"] === true) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>Welcome - Niggod</title>
     <link rel="icon" href="img/NiggodRat.ico">
     <link rel="stylesheet" href="css/welcome.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
-<header>
-    <h1>Welcome to NIGGOD</h1>
-</header>
-<main>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <a href="user/login.php" id="log-welcome">Log In</a>
-                <p>or</p>
-                <a href="user/signup.php" id="sign-welcome">Sign Up</a>
+    <header>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <!-- Logo -->
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.png" alt="" width="180" height="70" class="d-inline-block align-text-top">
+                </a>
+                <!-- Heading -->
+                <div>
+                    <h1>Welcome to NIGGOD</h1>
+                </div>
+                <!-- Buttons for "sign up" and "log in" -->
+                <div>
+                    <a href="user/signup.php" class="btn btn-dark linkForButtons">
+                        Sign Up
+                    </a>
+                    <a href="user/login.php" class="btn btn-secondary linkForButtons">
+                        Log In
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <a href="user/login.php" id="log-welcome">Log In</a>
+                    <p>or</p>
+                    <a href="user/signup.php" id="sign-welcome">Sign Up</a>
+                </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
+    <footer>
+
+
+    </footer>
 </body>
+
 </html>
