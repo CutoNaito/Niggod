166e8abc0d11816e0c3d7d3b2d138515167815b0
NNemec3 <neightn7@gmail.com>
2022-06-10 19:59:55 +0200

    Add icon

--- welcome.php
+++ welcome.php
@@ -11,6 +11,7 @@ if (isset($_SESSION["logged"]) && $_SESSION["logged"] === true) {
 <head>
     <meta charset="utf-8">
     <title>Welcome - Niggod</title>
+    <link rel="icon" href="img/NiggodRat.ico">
     <link rel="stylesheet" href="css/welcome.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
