50d07ea20312c00e517d0f2d93e2164540270028
NNemec3 <neightn7@gmail.com>
2022-06-11 19:19:51 +0200

    Style for poster

--- index.php
+++ index.php
@@ -25,7 +25,6 @@ if ($stmt = $conn->prepare($sql)) {
     <script src="script/index-script.ts"></script>
     <link rel="icon" href="../img/NiggodRat.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <script src="js/search.js"></script>
 </head>
 
 <body>
@@ -61,6 +60,35 @@ if ($stmt = $conn->prepare($sql)) {
     </header>
     <main>
         <div class="container" id="main_container">
+            <div class="card bg-dark text-white">
+                <div class="card-body">
+                    <form action="user/post.php" method="post" enctype="multipart/form-data">
+                        <div class="row text-center">
+                            <h2 class="card-title">What's on your mind?</h2>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3 mt-3">
+                                <textarea class="form-control" rows="5" id="comment" name="post-input"></textarea>
+                            </div>
+                            <div style="overflow: hidden; height: 0">
+                                <input type="file" id="fileInput" name="fileInput">
+                            </div>
+                            <button class="btn btn-outline-light" type="button" id="chooseImg" onclick="chooseFile()">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+                                    <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                                    <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="row">
+                            <div class="text-center">
+                                <button class="btn btn-outline-light my-2 my-sm-0" type="submit" id="post-submit">Post</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!--
             <div class="post-window">
                 <form action="user/post.php" method="post" enctype="multipart/form-data">
                     <div class="row">
@@ -86,6 +114,7 @@ if ($stmt = $conn->prepare($sql)) {
                     </div>
                 </form>
             </div>
+-->
             <div class="post-select">
                 <div class="row">
                     <div class="col">
@@ -137,7 +166,7 @@ if ($stmt = $conn->prepare($sql)) {
                     </svg>
                 </a>
                 <!-- Discord -->
-                <a class="btn btn-outline-light btn-floating m-1" href="https://discord.gg/qAtGbg4jfp!" role="button">
+                <a class="btn btn-outline-light btn-floating m-1" href="https://discord.gg/qAtGbg4jfp" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-discord" viewBox="0 0 16 16">
                         <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" />
                     </svg>
