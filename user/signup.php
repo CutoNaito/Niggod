3f8d73e4ab1d914829aab827e38289ce53617b63
NNemec3 <neightn7@gmail.com>
2022-07-11 18:31:32 +0200

    fix
    first icon in footer;
    remove prof. and home buttons from sing up and log in;
    responsive icons in welcome

--- user/signup.php
+++ user/signup.php
@@ -66,17 +66,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <nav class="navbar bg-light shadow mb-5 rounded">
             <div class="container-fluid">
                 <!-- Logo -->
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="../index.php">
                     <img src="../img/logo.png" alt="" width="180" height="70" class="d-inline-block align-text-top">
                 </a>
                 <!-- Buttons for "sign up" and "log in" with buttons for "home" and "profile"-->
                 <div>
-                    <a href="../index.php" class="btn btn-light linkForButtons">
-                        <img id="header_home_img" src="https://cdn.discordapp.com/attachments/943544446551752746/973269731534577694/unknown.png" alt="home">
-                    </a>
-                    <a href="#" class="btn btn-light linkForButtons">
-                        <img id="header_profile_img" src="https://cdn.discordapp.com/attachments/943544446551752746/973272366648021052/unknown.png" alt="profile">
-                    </a>
                     <a href="signup.php" class="btn btn-dark linkForButtons">
                         Sign Up
                     </a>
@@ -147,11 +141,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <section class="mb-4">
                 <!-- Facebook -->
                 <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                    <div class="iconSize1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-facebook iconSize1" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                     </svg>
-                    </div>
                 </a>
                 <!-- Twitter -->
                 <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
