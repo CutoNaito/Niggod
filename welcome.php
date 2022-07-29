d310ebba13022c3974d594d2227e8397637d59d6
NNemec3 <neightn7@gmail.com>
2022-07-30 01:13:05 +0200

    update search caards

--- welcome.php
+++ welcome.php
@@ -16,16 +16,13 @@ if (isset($_SESSION["logged"]) && $_SESSION["logged"] === true) {
     <link rel="stylesheet" href="css/welcome.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
-
         body {
             margin: 0;
             padding: 0;
         }
-
         .box {
             width: auto;
         }
-
         .box img {
             max-width: 100%;
             width: 100%;
