33a2e518360e0371faf8b094dd709bd1ba35d147
NNemec3 <neightn7@gmail.com>
2022-09-05 00:06:44 +0200

    index & profile pfp

--- profile.php
+++ profile.php
@@ -40,10 +40,10 @@ if ($stmt = $conn->prepare($sql)) {
                     $friend_caption = "Friend Request Sent";
                 }
             }
-    } else {
-        $friend_caption = "Add Friend";
+        } else {
+            $friend_caption = "Add Friend";
+        }
     }
-}
 }
 $sql = "SELECT * FROM post WHERE user_id = ? ORDER BY posted_at DESC";
 if ($stmt = $conn->prepare($sql)) {
@@ -61,91 +61,94 @@ if ($stmt = $conn->prepare($sql)) {
     <title>Profile - Niggod</title>
     <link rel="icon" href="img/NiggodRat.ico">
     <link rel="stylesheet" href="css/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
-<?php include("header.php"); ?>
-<main>
+    <?php include("header.php"); ?>
+    <main>
 
-    <div class="container">
-        <?php if ($_GET["username"] == $_SESSION["username"]) { ?>
-            <div class="card bg-dark text-white positionEditCard" style="width: 20%;">
-                <div class="card-body">
-                    <a href="user/editprofile.php">
-                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
-                            Edit
-                        </button>
+        <div class="container">
+            <?php if ($_GET["username"] == $_SESSION["username"]) { ?>
+                <div class="card bg-dark text-white positionEditCard" style="width: 20%;">
+                    <div class="card-body">
+                        <a href="user/editprofile.php">
+                            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+                                Edit
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            <?php } ?>
+            <?php if ($_GET["username"] != $_SESSION["username"]) { ?>
+                <div class="card bg-dark text-white positionEditCard" style="width: 20%;">
+                    <div class="card-body">
+                        <?php if ($friend_caption != "Friend Request Sent") { ?>
+                            <a href="user/addfriend.php?username=<?php echo $_GET["username"] ?>">
+                                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+                                    <?php echo $friend_caption ?>
+                                </button>
+                            </a>
+                        <?php } else {
+                            echo $friend_caption;
+                        } ?>
+                    </div>
+                </div>
+            <?php } ?>
+            <div class="container card-size">
+                <div class="position-relative">
+                    <a href="#">
+                        <img class="position-absolute positionPI csPI rounded" src="img/<?php echo $profile_picture ?>" alt="Profile picture"> <!-- absolute profile picture -->
                     </a>
                 </div>
-            </div>
-        <?php } ?>
-        <?php if ($_GET["username"] != $_SESSION["username"]) { ?>
-        <div class="card bg-dark text-white positionEditCard" style="width: 20%;">
-            <div class="card-body">
-                <?php if ($friend_caption != "Friend Request Sent") { ?>
-                <a href="user/addfriend.php?username=<?php echo $_GET["username"] ?>">
-                    <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
-                        <?php echo $friend_caption ?>
-                    </button>
-                </a>
-                <?php } else {
-                echo $friend_caption;
-                } ?>
-            </div>
-        </div>
-        <?php } ?>
-        <div class="container position-relative card-size">
-            <img class="position-absolute positionPI rounded"
-                 src="img/<?php echo $profile_picture ?>" alt="Profile picture"> <!-- absolute profile picture -->
-            <div class="card bg-dark text-white mt-2 mb-5">
-                <div class="card-body">
-                    <h1 class="card-title"><?php echo $username ?></h1>
-                    <p id="profile-register">Registered to Niggod: <?php echo $date ?></p>
-                    <p id="bio"><?php echo $bio ?></p>
+                <div class="card bg-dark text-white mt-2 mb-5">
+                    <div class="card-body">
+                        <h1 class="card-title"><?php echo $username ?></h1>
+                        <p id="profile-register">Registered to Niggod: <?php echo $date ?></p>
+                        <p id="bio"><?php echo $bio ?></p>
+                    </div>
                 </div>
             </div>
+
+            <?php
+            if (!empty($result)) {
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+            ?>
+                        <div class="container card-size">
+                            <div class="position-relative">
+                                <a href="#">
+                                    <img class="position-absolute positionPI csPI rounded" src="img/<?php echo $profile_picture ?>" alt="Profile picture">
+                                    <!-- absolute profile picture -->
+                                </a>
+                            </div>
+                            <div class="card bg-dark text-white mt-2 mb-2">
+                                <div class="card-body">
+                                    <a href="#" class="text-decoration-none text-white">
+                                        <h2 class="card-title"><?php echo $username ?></h2>
+                                    </a>
+                                    <p class="card-text"><?php echo $row["text_content"] ?></p>
+                                </div>
+                                <?php if ($row["image_content"] != "") { ?>
+                                    <img src="images/<?php echo $row["image_content"] ?>">
+                                <?php } ?>
+                                <div class="card-footer text-muted text-center">
+                                    <p class="marginZero">Posted at: <?php echo $row["posted_at"] ?></p>
+                                </div>
+                            </div>
+                        </div>
+            <?php
+                    }
+                } else {
+                    echo "0 results";
+                }
+            }
+            ?>
+
+
         </div>
-
-        <?php
-        if (!empty($result)) {
-        if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-        ?>
-        <div class="container position-relative card-size">
-            <a href="#">
-                <img class="position-absolute positionPI rounded"
-                     src="img/<?php echo $profile_picture ?>" alt="Profile picture">
-                <!-- absolute profile picture -->
-            </a>
-            <div class="card bg-dark text-white mt-2 mb-2">
-                <div class="card-body">
-                    <a href="#" class="text-decoration-none text-white">
-                        <h2 class="card-title"><?php echo $username ?></h2>
-                    </a>
-                    <p class="card-text"><?php echo $row["text_content"] ?></p>
-                </div>
-                <?php if($row["image_content"] != "") { ?>
-                <img src="images/<?php echo $row["image_content"] ?>">
-                <?php } ?>
-                <div class="card-footer text-muted text-center">
-                    <p class="marginZero">Posted at: <?php echo $row["posted_at"] ?></p>
-                </div>
-            </div>
-        </div>
-        <?php
-        }
-        } else {
-        echo "0 results";
-        }
-        }
-        ?>
-
-
-    </div>
-</main>
-<?php include("footer.php"); ?>
+    </main>
+    <?php include("footer.php"); ?>
 </body>
 
 </html>
