175c853e5405f92863a8300b777648d1d369bfa8
NNemec3 <neightn7@gmail.com>
2022-09-05 21:03:09 +0200

    smejus

--- index.php
+++ index.php
@@ -114,25 +114,34 @@ function checkIfFriend($username)
                                         <img class="position-absolute positionPI csPI rounded" src="img/<?php echo $row["profile_picture"] ?>" alt="Profile picture"> <!-- absolute profile picture -->
                                     </a>
                                 </div>
-                                
+
                                 <div class="card bg-dark text-white mt-2 mb-2">
+
                                     <div class="card-body">
                                         <a href="profile.php?username=<?php echo $row["username"] ?>" class="text-decoration-none text-white">
                                             <h2 class="card-title text-left"><?php echo $row["username"] ?></h2>
                                         </a>
                                         <p class="card-text"><?php echo $row["text_content"] ?></p>
                                     </div>
-                                    <?php if ($row["image_content"] != "")
-                                            {
-                                                if(str_contains($row["image_content"], ".mp4") ||str_contains($row["image_content"], ".webm"))
-                                                {?>
-                                                    <video src="images/<?php echo $row["image_content"] ?>" controls></video>
-                                                    <?php } else{?>
-                                                    <img src="images/<?php echo $row["image_content"] ?>">
-                                                <?php }
-                                            }{ ?>
+                                    <?php if ($row["image_content"] != "") {
+                                        if (str_contains($row["image_content"], ".mp4") || str_contains($row["image_content"], ".webm")) { ?>
+                                            <video src="images/<?php echo $row["image_content"] ?>" controls></video>
+                                        <?php } else { ?>
+                                            <img src="images/<?php echo $row["image_content"] ?>">
+                                        <?php }
+                                    } { ?>
                                     <?php } ?>
                                     <div class="card-footer text-muted text-center">
+                                        <div class="position-relative">
+                                            <div class="position-absolute positionLike">
+                                                <span class="rounded-pill spanPill">
+                                                    <a href="./user/like.php?postId=<?php echo $row["id"] ?>">
+                                                        <i class="bi bi-fire"></i>
+                                                        <i><?php echo $row["like_count"] ?></i>
+                                                    </a>
+                                                </span>
+                                            </div>
+                                        </div>
                                         <p class="marginZero">Posted at: <?php echo $row["posted_at"] ?></p>
                                     </div>
                                 </div>
