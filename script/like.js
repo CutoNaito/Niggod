82e3da1790947409ec9335f75c41a9ad83ffc503
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2022-09-05 22:07:40 +0200

    Update like.js

--- script/like.js
+++ script/like.js
@@ -1,4 +1,6 @@
 async function likeFunc(postId, userId) {
+    console.log(postId);
+    console.log(userId);
     let likesButton = document.getElementById(`like${postId}`);
     let likes = Number(likesButton.innerText);
     let response = await fetch(`../niggod/user/like.php?postId=${postId}&userId=${userId}`, {
@@ -7,10 +9,12 @@ async function likeFunc(postId, userId) {
         .then(response => response.json())
         .then(data => {
             if(data["type"] == 1) {
+                console.log(data)
                 likesButton.innerHTML = `<i class=\"bi bi-fire\"></i> ${likes + 1}`;
             }
             else
             {
+                console.log(data)
                 likesButton.innerHTML = `<i class=\"bi bi-fire\"></i> ${likes - 1}`;
             }
     });
