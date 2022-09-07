bd4dfdee90929ef89246b0bcf62a68092ec01109
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2022-09-07 11:21:38 +0200

    FIX LIKE!

--- script/like.js
+++ script/like.js
@@ -1,17 +1,22 @@
 async function likeFunc(postId, userId) {
     let likesButton = document.getElementById(`like${postId}`);
     let likes = Number(likesButton.innerText);
-    let response = await fetch(`../niggod/user/like.php?postId=${postId}&userId=${userId}`, {
+    let response = await fetch(`../user/like.php?postId=${postId}`, {
         method: "POST"
     })
         .then(response => response.json())
         .then(data => {
-            if(data["type"] == 1) {
-                likesButton.innerHTML = `<i class=\"bi bi-fire\"></i> ${likes + 1}`;
-            }
-            else
-            {
-                likesButton.innerHTML = `<i class=\"bi bi-fire\"></i> ${likes - 1}`;
+            switch (data["type"]) {
+                case 0:
+                    likesButton.innerHTML = `<i class=\"bi bi-fire\"></i> ${likes - 1}`;
+                    break;
+
+                case 1:
+                    likesButton.innerHTML = `<i class=\"bi bi-fire\"></i> ${likes + 1}`;
+                    break;
+
+                default:
+                    break;
             }
     });
 }
