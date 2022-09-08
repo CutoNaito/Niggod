5242cffe5abfc682a11f13ab5164066176241216
NNemec3 <neightn7@gmail.com>
2022-09-09 00:09:24 +0200

    smejus

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ function checkIfFriend($username)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Home - Niggod</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="script/index-script.ts"></script>
+    <script src="script/index-script.js"></script>
     <script src="script/like.js"></script>
     <link rel="icon" href="img/NiggodRat.ico">
     <link href="https://vjs.zencdn.net/7.20.2/video-js.css" rel="stylesheet" />
@@ -134,7 +134,7 @@ function checkIfFriend($username)
                                     <p class="marginZero">Posted at: <?php echo $row["posted_at"] ?></p>
                                 </div>
                             </div>
-                            
+
             <?php
                         }
                     }
--- user/editprofile.php
+++ user/editprofile.php
@@ -25,7 +25,7 @@ if ($stmt = $conn->prepare($sql)) {
     <link rel="stylesheet" href="../css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <script src="../script/index-script.ts"></script>
+    <script src="../script/index-script.js"></script>
 </head>
 
 <body>
